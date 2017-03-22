d95a69b053bbe7f2fc9acd04b2be7e3e4defe8c7
kinkorea <alexkarasik89@gmail.com>
2017-03-22 09:32:16 -0400

    Ember Object submission
    I spent too much time on trying to figure out some linter errors that
    did not make sense to me, but also definitely need to review this
    material.
    
    Comfort: 2
    Clarity: 2

--- public/diagnostic.js
+++ public/diagnostic.js
@@ -7,6 +7,33 @@
 //  -  a unit price
 //  -  a quantity
 //  -  a computed property called `orderPrice`, equal to price times quantity
+
+const Order = Ember.Object.extend({
+  let price = {
+    price: 5,
+    get: function(){
+      return this.price;
+    }
+  },
+  set: function(newVal){
+    this.count(newVal);
+    return this.
+  };
+
+  let quantity = {
+    quantitiy: 5,
+    get: function(){
+      return this.quantity;
+    }
+  },
+  set: function(newVal){
+    this.count(newVal);
+    return this.
+  };
+  orderPrice: Ember.computed('price', 'quantity', function(){
+    return `${this.get('price')} * ${this.get('quantity')}`
+  })
+});
 //
 // A Cart should have
 //  -  an `addToCart` method, which adds a given Item to an array
@@ -16,6 +43,15 @@
 //      the `orderPrice` values for everything in the cart); it should be
 //      recalculated any time an Order is added to the cart, removed from the
 //      cart, or modified.
+
+const Cart = Ember.Object.extend({
+  let orders = [],
+  addToCart: Ember.computed(),
+
+  totalPrice: Ember.computed(),
+});
+
+
 //
 // Once you've created the necessary Ember Classes, create a new Cart instance,
 //  and fill that cart up with three new product orders having the following
